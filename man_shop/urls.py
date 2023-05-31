a5aeb8a58210a85cdf03dd3e6613cb88fede1267
verzek02 <murataalievu@gmail.com>
2023-05-31 21:22:38 +0600

    Сделал 5 дз

--- man_shop/urls.py
+++ man_shop/urls.py
@@ -5,9 +5,10 @@ from man_shop import views
 
 
 urlpatterns = [
-    path('', views.product_view, name='product'),
-    path('product_detail/<int:id>/', views.product_detail, name='product_detail'),
-    path('product_detail/<int:id>/delete/', views.delete_product, name='delete_product'),
-    path('product_detail/<int:id>/update/', views.update_product, name='update_product'),
-    path('add-product/', views.create_product_view, name='create_product')
+    path('', views.PersonView.as_view(), name='product'),
+    path('product_detail/<int:id>/', views.PersonDetailView.as_view(), name='product_detail'),
+    path('product_detail/<int:id>/delete/', views.DeleteProductView.as_view(), name='delete_product'),
+    path('product_detail/<int:id>/update/', views.UpdateProductView.as_view(), name='update_product'),
+    path('add-product/', views.CreateProductView.as_view(), name='create_product'),
+    path('search/', views.Search.as_view(), name='search'),
 ]
--- man_shop/views.py
+++ man_shop/views.py
@@ -1,58 +1,116 @@
 from django.http import HttpResponse
 from django.shortcuts import render,get_object_or_404
 from man_shop import models,forms
+from django.views import generic
+
 
 #Неполная информация
-def product_view(request):
-    product = models.Man_shop.objects.all()
-    return render(request, 'product/product.html', {'product': product})
+class PersonView(generic.ListView):
+    template_name = 'product/product.html'
+    queryset = models.Man_shop.objects.all()
+
+    def get_queryset(self):
+        return models.Man_shop.objects.all()
+
+# def product_view(request):
+#     product = models.Man_shop.objects.all()
+#     return render(request, 'product/product.html', {'product': product})
 
 #Полная инофрмация
+class PersonDetailView(generic.DetailView):
+    template_name = 'product/product_detail.html'
 
-def product_detail(request,id):
-    product_id = get_object_or_404(models.Man_shop, id=id)
-    return render(request, 'product/product_detail.html', {'product_id': product_id})
+    def get_object(self, **kwargs):
+        person_id = self.kwargs.get('id')
+        return get_object_or_404(models.Man_shop, id=person_id)
+
+# def product_detail(request,id):
+#     product_id = get_object_or_404(models.Man_shop, id=id)
+#     return render(request, 'product/product_detail.html', {'product_id': product_id})
 
 
 #Добавление товара
-def create_product_view(request):
-    method = request.method
-    if method == "POST":
-        form = forms.Man_shop_form(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Товар успешно добавлен!')
-    else:
-        form = forms.Man_shop_form()
-    return render(request, "crud/create_product.html", {'form': form})
+class CreateProductView(generic.CreateView):
+    template_name = "crud/create_product.html"
+    form_class = forms.Man_shop_form
+    queryset = models.Man_shop.objects.all()
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(CreateProductView, self).form_valid(form=form)
+# def create_product_view(request):
+#     method = request.method
+#     if method == "POST":
+#         form = forms.Man_shop_form(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Товар успешно добавлен!')
+#     else:
+#         form = forms.Man_shop_form()
+#     return render(request, "crud/create_product.html", {'form': form})
 
 
 
 #Удаление товара
 
-def delete_product(request, id):
-    product_id = get_object_or_404(models.Man_shop, id=id)
-    product_id.delete()
-    return HttpResponse('Товар удален')
+class DeleteProductView(generic.DeleteView):
+    template_name = 'CRUD/confirm_delete.html'
+    success_url = '/'
+
+    def get_object(self, **kwargs):
+        person_id = self.kwargs.get('id')
+        return get_object_or_404(models.Man_shop, id=person_id)
+
+
+# def delete_product(request, id):
+#     product_id = get_object_or_404(models.Man_shop, id=id)
+#     product_id.delete()
+#     return HttpResponse('Товар удален')
 
 
 #Обновление товара
+class UpdateProductView(generic.UpdateView):
+    template_name = 'CRUD/update_product.html'
+    form_class = forms.Man_shop_form
+    success_url = '/'
 
-def update_product(request,id):
-    product_id = get_object_or_404(models.Man_shop, id=id)
-    if request.method == 'POST':
-        form = forms.Man_shop_form(instance=product_id, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Товар успешно изменён')
-    else:
-        form = forms.Man_shop_form(instance=product_id)
-    context = {
-        'form': form,
-        'product_id': product_id
-    }
-    return render(request, 'CRUD/update_product.html', context)
+    def get_object(self, **kwargs):
+        person_id = self.kwargs.get('id')
+        return get_object_or_404(models.Man_shop, id=person_id)
 
+    def form_valid(self, form):
+        return super(UpdateProductView, self).form_valid(form=form)
+
+
+
+# def update_product(request,id):
+#     product_id = get_object_or_404(models.Man_shop, id=id)
+#     if request.method == 'POST':
+#         form = forms.Man_shop_form(instance=product_id, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Товар успешно изменён')
+#     else:
+#         form = forms.Man_shop_form(instance=product_id)
+#     context = {
+#         'form': form,
+#         'product_id': product_id
+#     }
+#     return render(request, 'CRUD/update_product.html', context)
+
+class Search(generic.ListView):
+    template_name = 'product/product.html'
+    context_object_name = 'product'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Man_shop.objects.filter(product_name__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
 
 
 
